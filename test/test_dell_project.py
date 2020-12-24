5a4ff5e8b290efb80ec6b2062cff552c6c8da9ff
ibelyanova <ir-bell@mail.ru>
2020-12-24 19:27:11 +0300

    Задание 25 добавление проверок + предусловия

--- test/test_dell_project.py
+++ test/test_dell_project.py
@@ -1,9 +1,10 @@
 import random
 
 def test_dell_project(app, db, json_project):
-    if len(db.get_project_list())  == 0:
-        app.project.create_new_project(json_project)
     app.session.login("administrator", "root")
+    app.project.open_project_page()
+    if len(db.get_project_list()) == 0:
+        app.project.create_new_project(json_project)
     app.project.open_project_page()
     old_project = db.get_project_list()
     project = random.choice(old_project)
